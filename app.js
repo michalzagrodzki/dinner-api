ff8e7f24be7f15d01244dbe219bc53dddca83495
Michal Zagrodzki <michalzagrodzki@gmail.com>
2021-07-26 15:00:25 +0200

    Add local console.err method

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
+  console.error(err.stack);
   // send error response
   res.status(err.status || 500).json(err.message);
 });
