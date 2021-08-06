e2f9be092b670f23c3356a539639c41affd088bc
Michal Zagrodzki <michalzagrodzki@gmail.com>
2021-08-06 15:21:29 +0200

    chore: Add mockup data list

---
+++ mockup/dinners.js
@@ -0,0 +1,23 @@
+const dinnersList = [
+  {
+    title: "Pasta",
+    price: "5.49",
+    weight: 200,
+    calories: 500,
+    ingredients: [],
+  },
+  {
+    title: "Pizza",
+    price: "6.25",
+    weight: 180,
+    calories: 450,
+    ingredients: [],
+  },
+  {
+    title: "Risotto",
+    price: "4.70",
+    weight: 160,
+    calories: 400,
+    ingredients: [],
+  },
+];
---
+++ mockup/ingredients.js
@@ -0,0 +1,87 @@
+const ingredientsList = [
+  {
+    name: "eggs",
+    price: "1.48",
+    calories: 155,
+  },
+  {
+    name: "flour",
+    price: "1.02",
+    calories: 364,
+  },
+  {
+    name: "olive oil",
+    price: "3.89",
+    calories: 884,
+  },
+  {
+    name: "salt",
+    price: "0.24",
+    calories: 0,
+  },
+  {
+    name: "beef",
+    price: "2.71",
+    calories: 250,
+  },
+  {
+    name: "onion",
+    price: "0.22",
+    calories: 40,
+  },
+  {
+    name: "basil",
+    price: "0.92",
+    calories: 22,
+  },
+  {
+    name: "oregano",
+    price: "0.90",
+    calories: 27,
+  },
+  {
+    name: "tomato sauce",
+    price: "1.69",
+    calories: 29,
+  },
+  {
+    name: "cheddar cheese",
+    price: "5.54",
+    calories: 402,
+  },
+  {
+    name: "chicken",
+    price: "2.31",
+    calories: 239,
+  },
+  {
+    name: "shrimps",
+    price: "10.47",
+    calories: 99,
+  },
+  {
+    name: "garlic",
+    price: "0.76",
+    calories: 5,
+  },
+  {
+    name: "celery",
+    price: "0.22",
+    calories: 6,
+  },
+  {
+    name: "parmesan cheese",
+    price: "11",
+    calories: 431,
+  },
+  {
+    name: "rice",
+    price: "1.33",
+    calories: 130,
+  },
+  {
+    name: "butter",
+    price: "0.14",
+    calories: 717,
+  },
+];
