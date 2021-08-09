9162f7d2c6aa1545f6b5677ac52d45b8e771f651
Michal Zagrodzki <michalzagrodzki@gmail.com>
2021-08-09 14:58:40 +0200

    refactor: update model for Orders

--- domains/orders/orders.model.js
+++ domains/orders/orders.model.js
@@ -4,8 +4,12 @@ const { Schema } = mongoose;
 const orderSchema = new Schema({
   title: { type: String },
   dinner_id: { type: Schema.Types.ObjectId, ref: "Dinner" },
+  client_name: { type: String, required: true },
+  client_phone: { type: String, required: true },
+  client_email: { type: String },
   price: { type: String, required: true, min: 0 },
   calories: { type: Number, required: true, min: 0 },
+  weight: { type: Number },
   ingredients: [{ type: Schema.Types.ObjectId, ref: "Ingredient" }],
 });
 
