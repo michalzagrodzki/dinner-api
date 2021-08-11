c8eb9e39b5314b05715856c9a30a68b9680148ae
Michal Zagrodzki <michalzagrodzki@gmail.com>
2021-08-11 17:53:59 +0200

    feat: add method for saving order to database

--- domains/orders/orders.service.js
+++ domains/orders/orders.service.js
@@ -19,7 +19,37 @@ service.getDetails = async (id) => {
 };
 
 service.postOrder = async (payload) => {
-  const response = "post order;";
+  const {
+    title,
+    dinner_id,
+    client_name,
+    client_phone,
+    client_email,
+    evaluatedPrice: price,
+    parseCalories: calories,
+    parseWeight: weight,
+    parsedIngredients: ingredients,
+  } = payload;
+  const orderRecord = new Order({
+    title,
+    dinner_id,
+    client_name,
+    client_phone,
+    client_email,
+    price,
+    calories,
+    weight,
+    ingredients,
+  });
+  await orderRecord.save(function (err) {
+    if (err) return err;
+  });
+  const response = {
+    client_name,
+    client_phone,
+    client_email,
+  };
+
   return response;
 };
 
