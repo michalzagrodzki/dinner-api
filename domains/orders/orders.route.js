25dbd86478aa77379be83f31f6d2dcac2c18240a
Michal Zagrodzki <michalzagrodzki@gmail.com>
2021-08-14 08:52:06 +0200

    feat: Create outline for swagger sections ingredients and orders

--- domains/orders/orders.route.js
+++ domains/orders/orders.route.js
@@ -11,3 +11,55 @@ router.get("/details/:id", ordersValidator.get, ordersController.get);
 router.post("/", ordersValidator.post, ordersController.post);
 
 module.exports = router;
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     NewOrder:
+ *       type: object
+ *       required:
+ *         - name
+ *       properties:
+ *         name:
+ *           type: string
+ *           description: The user's name.
+ *           example: John John
+ *     Order:
+ *       allOf:
+ *         - type: object
+ *           properties:
+ *             id:
+ *               type: integer
+ *               description: The dinner ID.
+ *               example: 0
+ *         - $ref: '#/components/schemas/Dinner'
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Orders
+ *   description: API for orders in the system.
+ */
+
+/**
+ * @swagger
+ * /orders:
+ *   get:
+ *     summary: Retrieve a list of Orders.
+ *     tags: [Orders]
+ *     description: Retrieve a list of orders from Dinners API.
+ *     responses:
+ *       200:
+ *         description: A list of orders.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/Orders'
+ */
