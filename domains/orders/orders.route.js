6e3b8a0bbf0f07d114c99e1f05b45eec872e831c
Michal Zagrodzki <michalzagrodzki@gmail.com>
2021-08-18 16:02:45 +0200

    style: change layout of order routes code

--- domains/orders/orders.route.js
+++ domains/orders/orders.route.js
@@ -6,12 +6,14 @@ const ordersController = require("./orders.controller");
 // Validators
 const { ordersValidator } = require("./orders.middleware");
 
-router.get("/", ordersController.list);
+router
+  .route("/")
+  .get(ordersController.list)
+  .post(ordersValidator.post, ordersController.post);
 router
   .route("/details/:id")
   .get(ordersValidator.get, ordersController.get)
   .put(ordersValidator.post, ordersController.put)
   .delete(ordersValidator.get, ordersController.delete);
-router.post("/", ordersValidator.post, ordersController.post);
 
 module.exports = router;
